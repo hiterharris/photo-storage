d6b376b409f985531f892fe1a674c526c09f977d
Henry H. Harris (Signature) <hharris@MacBook-Pro-2.local>
2020-09-25 17:37:22 -0400

    uplod image success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import axios from 'axios';
 
 function App() {
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = e => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="App">
-      Photo Storage
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        style={{
+          display: "none"
+        }}
+      />
+      <div
+        style={{
+          height: "250px",
+          width: "250px",
+          border: "1px dashed black"
+        }}
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </div>
+      Click to upload Image
     </div>
   );
 }
